d6d0839791dd98d22281b7c3c5f12d682ab371e5
rzmoz <rzmoz@users.noreply.github.com>
2012-08-15 16:36:13 +0200

    added curl for use with SDN download

---
+++ Trunk/SitecoreInstaller.Framework/Network/Curl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SitecoreInstaller.Framework.Network
+{
+    using SitecoreInstaller.Framework.System;
+
+    using global::System.Diagnostics.Contracts;
+    using global::System.IO;
+
+    public class Curl : CommandPrompt
+    {
+        private const string _FileName = @"curl.exe";
+        private const string _Format = _FileName + @" ";
+
+        public Curl(DirectoryInfo targetFolder)
+        {
+            TargetFolder = targetFolder;
+        }
+
+        public void Download(string url)
+        {
+            Contract.Requires<ArgumentNullException>(url != null);
+        }
+
+        public DirectoryInfo TargetFolder { get; private set; }
+
+    }
+}
