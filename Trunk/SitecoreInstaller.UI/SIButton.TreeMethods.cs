17560f686a1aba3a816bd48ae5b5d15a08c6dca3
rzmoz <rzmoz@users.noreply.github.com>
2013-02-25 21:14:18 +0100

    cc

---
+++ Trunk/SitecoreInstaller.UI/SIButton.TreeMethods.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SitecoreInstaller.UI
+{
+  public partial class SIButton
+  {
+    public string Path
+    {
+      get { return this.GetPath(this); }
+    }
+
+    /// <summary>
+    /// zero based
+    /// </summary>
+    public int Level
+    {
+      get { return this.GetLevel(this); }
+    }
+
+    public IEnumerable<SIButton> SubButtons
+    {
+      get { return this.Controls.OfType<SIButton>(); }
+    }
+    public SIButton ParentButton
+    {
+      get { return this.Parent as SIButton; }
+    }
+
+    public bool IsRoot
+    {
+      get
+      {
+        if (this.Name.Length == 0)
+          return true;
+        if (this.Parent == null)
+          return true;
+        if (this.Parent as SIButton == null)
+          return true;
+        return false;
+      }
+    }
+
+    private string GetPath(SIButton button)
+    {
+      if (button.IsRoot)
+        return string.Empty;
+
+      var myPath = "/" + Text.ToLower().Replace(" ", string.Empty);
+
+      return this.GetPath((SIButton)button.Parent) + myPath;
+    }
+
+    private int GetLevel(SIButton button)
+    {
+      if (button.IsRoot || button.ParentButton.IsRoot)
+        return 0;
+      return 1 + this.GetLevel(this.ParentButton);
+    }
+  }
+}
--- Trunk/SitecoreInstaller.UI/SiButton.cs
+++ Trunk/SitecoreInstaller.UI/SiButton.cs
@@ -9,7 +9,7 @@ namespace SitecoreInstaller.UI
   using System.Windows.Forms;
   using SitecoreInstaller.Framework.System;
 
-  public class SIButton : Button
+  public partial class SIButton : Button
   {
     private readonly Color InitColor = Color.Chartreuse; //this color equeals not set - hope no one uses this color ever!
 
@@ -20,60 +20,7 @@ namespace SitecoreInstaller.UI
 
     public event EventHandler<GenericEventArgs<SIButton>> Activated;
     public event EventHandler<GenericEventArgs<SIButton>> DeActivated;
-
-    public string Path
-    {
-      get { return this.GetPath(this); }
-    }
-    private string GetPath(SIButton button)
-    {
-      if (button.IsRoot)
-        return string.Empty;
-
-      var myPath = "/" + Text.ToLower().Replace(" ", string.Empty);
-
-      return this.GetPath((SIButton)button.Parent) + myPath;
-    }
-
-    /// <summary>
-    /// zero based
-    /// </summary>
-    public int Level
-    {
-      get { return this.GetLevel(this); }
-    }
-
-    private int GetLevel(SIButton button)
-    {
-      if (button.IsRoot || button.ParentButton.IsRoot)
-        return 0;
-      return 1 + this.GetLevel(this.ParentButton);
-    }
-
-    public IEnumerable<SIButton> SubButtons
-    {
-      get { return this.Controls.OfType<SIButton>(); }
-    }
-    public SIButton ParentButton
-    {
-      get { return this.Parent as SIButton; }
-    }
-    public bool IsRoot
-    {
-      get
-      {
-        if (this.Name.Length == 0)
-          return true;
-        if (this.Parent == null)
-          return true;
-        if (this.Parent as SIButton == null)
-          return true;
-        return false;
-      }
-    }
-
-
-
+    
     private void InitializeComponent()
     {
       SuspendLayout();
@@ -115,6 +62,7 @@ namespace SitecoreInstaller.UI
       if (Activated != null)
         Activated(this, new GenericEventArgs<SIButton>(this));
     }
+
     public void DeActivate()
     {
       if (BackColorNotSelected.Equals(InitColor))
