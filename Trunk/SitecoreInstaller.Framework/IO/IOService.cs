bef8f2840d8033e6c130cde1935eb35e65b4af37
rzmoz <rzmoz@users.noreply.github.com>
2013-05-13 16:34:36 +0200

    Fixed consolidate folders where exception was thrown when deleting source folder. Typically happened when a new sitecore was exrtacted and consolidated

--- Trunk/SitecoreInstaller.Framework/IO/IOService.cs
+++ Trunk/SitecoreInstaller.Framework/IO/IOService.cs
@@ -7,205 +7,203 @@ using System.Threading.Tasks;
 
 namespace SitecoreInstaller.Framework.IO
 {
-    using SitecoreInstaller.Framework.Diagnostics;
+  using SitecoreInstaller.Framework.Diagnostics;
+  using global::System.Diagnostics.Contracts;
 
-    using global::System.Diagnostics;
-    using global::System.Diagnostics.Contracts;
-
-    public static class IoService
+  public static class IoService
+  {
+    public static void WriteToDisk(this string content, FileInfo targetFile)
     {
-        public static void WriteToDisk(this string content, FileInfo targetFile)
-        {
-            if (targetFile == null)
-                return;
+      if (targetFile == null)
+        return;
 
-            targetFile.Directory.CreateIfNotExists();
+      targetFile.Directory.CreateIfNotExists();
 
-            File.WriteAllText(targetFile.FullName, content);
-            Log.This.Debug("Saved string to disk at: {0}", targetFile.FullName);
-        }
-        public static void WriteToDir(this string content, DirectoryInfo dir, string filename)
-        {
-            var file = new FileInfo(Path.Combine(dir.FullName, filename));
-            WriteToDisk(content, file);
-        }
-
-        public static IEnumerable<string> GetUniqueFileNames(this DirectoryInfo folder)
-        {
-            if (Directory.Exists(folder.FullName) == false)
-                yield break;
-
-            var uniqueFileNames = new HashSet<string>();
-
-            foreach (var file in folder.GetFiles())
-            {
-                var processedName = file.NameWithoutExtension().ToLowerInvariant();
-                if (uniqueFileNames.Contains(processedName) == false)
-                {
-                    uniqueFileNames.Add(processedName);
-                    yield return file.NameWithoutExtension();
-                }
-            }
-        }
-        public static void CreateWithLog(this DirectoryInfo folder)
-        {
-            if (Directory.Exists(folder.FullName))
-                Log.This.Debug("Folder already exist: '{0}'", folder.FullName);
-            else
-            {
-                folder.Create();
-                Log.This.Debug("Folder created: '{0}'", folder.FullName);
-            }
-        }
-        public static void DeleteWithLog(this DirectoryInfo folder)
-        {
-            const int retries = 10;
-
-            if (folder.Exists() == false)
-                return;
-
-            for (var tryCount = 1; tryCount <= retries; tryCount++)
-            {
-                try
-                {
-                    folder.Delete(true);
-                    Log.This.Debug("Folder deleted: '{0}'", folder.FullName);
-                }
-                catch (DirectoryNotFoundException)
-                {
-                    //happens when the files are released and deleted between retries
-                    break;
-                }
-                catch (UnauthorizedAccessException)
-                {
-                    Log.This.Debug("Waiting for iis to release file handles...");
-                    Thread.Sleep(500);
-                }
-                catch (IOException)
-                {
-                    Log.This.Debug("Waiting for iis to release file handles...");
-                    Thread.Sleep(500);
-                }
-                catch (SecurityException e)
-                {
-                    Log.This.Debug("Waiting for iis to release file handles...");
-                    Thread.Sleep(500);
-                }
-            }
-
-            if (folder.Exists())
-                Log.This.Warning("Gave up waiting. Please delete folder manually: '{0}'", folder.FullName);
-        }
-
-        public static void ConsolidateIdenticalSubfolders(this DirectoryInfo rootFolder)
-        {
-            if (rootFolder == null)
-                return;
-            if (rootFolder.Exists() == false)
-                return;
-
-            //if folder has a sub folder with identical name, we move it up one level
-            DirectoryInfo identicalSubFolder;
-
-            if (rootFolder.TryGetIdenticallyNamedSubDir(out identicalSubFolder))
-            {
-                Log.This.Debug("'{0}' contains identical named subfolder. Consolidating...", identicalSubFolder.FullName);
-                ConsolidateIdenticalSubfolders(identicalSubFolder);
-            }
-
-            if (rootFolder.ParentHasIdenticalName())
-            {
-                var robocopy = new Robocopy();
-                robocopy.Move(rootFolder, rootFolder.Parent);
-            }
-        }
-
-        public static bool TryBackup(this FileInfo file)
-        {
-            Log.This.Debug("Trying to backup file: {0}", file.FullName);
-
-            if (!File.Exists(file.FullName))
-            {
-                Log.This.Debug("File not found for backup: {0}", file.FullName);
-                return false;
-            }
-
-            string backupFileName;
-            do
-            {
-                backupFileName = GetBackupFileName(file);
-            }
-            while (File.Exists(backupFileName));
-
-            file.CopyTo(backupFileName);
-            Log.This.Debug("Existing file was backed up to :" + backupFileName);
-            return true;
-        }
-        private static string GetBackupFileName(FileInfo file)
-        {
-            Contract.Requires<ArgumentNullException>(file != null);
-
-            var backupExtension = "." + DateTime.Now.ToString("yyyy.MM.dd.HH.mm.ss") + ".backup";
-            return file.FullName + backupExtension;
-        }
-
-        public static IEnumerable<DirectoryInfo> GetSubFolders(this DirectoryInfo rootFolder, string subfoldersDescription = "")
-        {
-            if (rootFolder == null)
-                yield break;
-
-            Log.This.Debug("Getting {0}s", subfoldersDescription);
-            if (!rootFolder.Exists)
-            {
-                Log.This.Error(subfoldersDescription + " folder not found: {0}", rootFolder.FullName);
-                yield break;
-            }
-
-            foreach (var dir in rootFolder.GetDirectories(".", SearchOption.TopDirectoryOnly))
-            {
-                Log.This.Debug(subfoldersDescription + "s found: {0}", dir.Name);
-                yield return dir;
-            }
-        }
-        public static void CopyTo(this DirectoryInfo source, Folder target, DirCopyOptions dirCopyOptions)
-        {
-            source.CopyTo(target.Directory, dirCopyOptions);
-        }
-        public static void CopyTo(this DirectoryInfo source, DirectoryInfo target, DirCopyOptions dirCopyOptions)
-        {
-            if (Directory.Exists(source.FullName) == false)
-            {
-                Log.This.Debug("Source '{0}' not found. Aborting", source.FullName);
-                return;
-            }
-
-            if (source.FullName.Equals(target.FullName, StringComparison.OrdinalIgnoreCase))
-            {
-                Log.This.Debug("Source and Target are the same '{0}'. Aborting", source.FullName);
-                return;
-            }
-            var robocopy = new Robocopy();
-            robocopy.Copy(source, target, dirCopyOptions);
-        }
-
-        public static void CopyFlattenedTo(this DirectoryInfo source, DirectoryInfo target, string searchPattern = "*")
-        {
-            Log.This.Debug("Flattening '{0}' files in '{1}'...", searchPattern, target.FullName);
-            if (Directory.Exists(source.FullName) == false)
-            {
-                Log.This.Debug("Source '{0}' not found. Aborting", source.FullName, target.FullName);
-                return;
-            }
-
-            // Check if the target directory exists, if not, create it.
-            target.CreateIfNotExists();
-
-            // Copy each file into it’s new directory.
-            Parallel.ForEach(source.GetFiles(searchPattern, SearchOption.AllDirectories), file =>
-                                                                                                {
-                                                                                                    var targetFile = target.CombineTo<FileInfo>(file.Name);
-                                                                                                    file.CopyTo(targetFile.FullName, true);
-                                                                                                });
-        }
+      File.WriteAllText(targetFile.FullName, content);
+      Log.This.Debug("Saved string to disk at: {0}", targetFile.FullName);
     }
+    public static void WriteToDir(this string content, DirectoryInfo dir, string filename)
+    {
+      var file = new FileInfo(Path.Combine(dir.FullName, filename));
+      WriteToDisk(content, file);
+    }
+
+    public static IEnumerable<string> GetUniqueFileNames(this DirectoryInfo folder)
+    {
+      if (Directory.Exists(folder.FullName) == false)
+        yield break;
+
+      var uniqueFileNames = new HashSet<string>();
+
+      foreach (var file in folder.GetFiles())
+      {
+        var processedName = file.NameWithoutExtension().ToLowerInvariant();
+        if (uniqueFileNames.Contains(processedName) == false)
+        {
+          uniqueFileNames.Add(processedName);
+          yield return file.NameWithoutExtension();
+        }
+      }
+    }
+    public static void CreateWithLog(this DirectoryInfo folder)
+    {
+      if (Directory.Exists(folder.FullName))
+        Log.This.Debug("Folder already exist: '{0}'", folder.FullName);
+      else
+      {
+        folder.Create();
+        Log.This.Debug("Folder created: '{0}'", folder.FullName);
+      }
+    }
+    public static void DeleteWithLog(this DirectoryInfo folder)
+    {
+      const int retries = 10;
+
+      if (folder.Exists() == false)
+        return;
+
+      for (var tryCount = 1; tryCount <= retries; tryCount++)
+      {
+        try
+        {
+          folder.Delete(true);
+          Log.This.Debug("Folder deleted: '{0}'", folder.FullName);
+        }
+        catch (DirectoryNotFoundException)
+        {
+          //happens when the files are released and deleted between retries
+          break;
+        }
+        catch (UnauthorizedAccessException)
+        {
+          Log.This.Debug("Waiting for iis to release file handles...");
+          Thread.Sleep(500);
+        }
+        catch (IOException)
+        {
+          Log.This.Debug("Waiting for iis to release file handles...");
+          Thread.Sleep(500);
+        }
+        catch (SecurityException e)
+        {
+          Log.This.Debug("Waiting for iis to release file handles...");
+          Thread.Sleep(500);
+        }
+      }
+
+      if (folder.Exists())
+        Log.This.Warning("Gave up waiting. Please delete folder manually: '{0}'", folder.FullName);
+    }
+
+    public static void ConsolidateIdenticalSubfolders(this DirectoryInfo rootFolder)
+    {
+      if (rootFolder == null)
+        return;
+      if (rootFolder.Exists() == false)
+        return;
+
+      //if folder has a sub folder with identical name, we move it up one level
+      DirectoryInfo identicalSubFolder;
+
+      if (rootFolder.TryGetIdenticallyNamedSubDir(out identicalSubFolder))
+      {
+        Log.This.Debug("'{0}' contains identical named subfolder. Consolidating...", identicalSubFolder.FullName);
+        ConsolidateIdenticalSubfolders(identicalSubFolder);
+      }
+
+      if (rootFolder.ParentHasIdenticalName())
+      {
+        var robocopy = new Robocopy();
+        robocopy.Move(rootFolder, rootFolder.Parent);
+      }
+    }
+
+    public static bool TryBackup(this FileInfo file)
+    {
+      Log.This.Debug("Trying to backup file: {0}", file.FullName);
+
+      if (!File.Exists(file.FullName))
+      {
+        Log.This.Debug("File not found for backup: {0}", file.FullName);
+        return false;
+      }
+
+      string backupFileName;
+      do
+      {
+        backupFileName = GetBackupFileName(file);
+      }
+      while (File.Exists(backupFileName));
+
+      file.CopyTo(backupFileName);
+      Log.This.Debug("Existing file was backed up to :" + backupFileName);
+      return true;
+    }
+    private static string GetBackupFileName(FileInfo file)
+    {
+      Contract.Requires<ArgumentNullException>(file != null);
+
+      var backupExtension = "." + DateTime.Now.ToString("yyyy.MM.dd.HH.mm.ss") + ".backup";
+      return file.FullName + backupExtension;
+    }
+
+    public static IEnumerable<DirectoryInfo> GetSubFolders(this DirectoryInfo rootFolder, string subfoldersDescription = "")
+    {
+      if (rootFolder == null)
+        yield break;
+
+      Log.This.Debug("Getting {0}s", subfoldersDescription);
+      if (!rootFolder.Exists)
+      {
+        Log.This.Error(subfoldersDescription + " folder not found: {0}", rootFolder.FullName);
+        yield break;
+      }
+
+      foreach (var dir in rootFolder.GetDirectories(".", SearchOption.TopDirectoryOnly))
+      {
+        Log.This.Debug(subfoldersDescription + "s found: {0}", dir.Name);
+        yield return dir;
+      }
+    }
+    public static void CopyTo(this DirectoryInfo source, Folder target, DirCopyOptions dirCopyOptions)
+    {
+      source.CopyTo(target.Directory, dirCopyOptions);
+    }
+    public static void CopyTo(this DirectoryInfo source, DirectoryInfo target, DirCopyOptions dirCopyOptions)
+    {
+      if (Directory.Exists(source.FullName) == false)
+      {
+        Log.This.Debug("Source '{0}' not found. Aborting", source.FullName);
+        return;
+      }
+
+      if (source.FullName.Equals(target.FullName, StringComparison.OrdinalIgnoreCase))
+      {
+        Log.This.Debug("Source and Target are the same '{0}'. Aborting", source.FullName);
+        return;
+      }
+      var robocopy = new Robocopy();
+      robocopy.Copy(source, target, dirCopyOptions);
+    }
+
+    public static void CopyFlattenedTo(this DirectoryInfo source, DirectoryInfo target, string searchPattern = "*")
+    {
+      Log.This.Debug("Flattening '{0}' files in '{1}'...", searchPattern, target.FullName);
+      if (Directory.Exists(source.FullName) == false)
+      {
+        Log.This.Debug("Source '{0}' not found. Aborting", source.FullName, target.FullName);
+        return;
+      }
+
+      // Check if the target directory exists, if not, create it.
+      target.CreateIfNotExists();
+
+      // Copy each file into it’s new directory.
+      Parallel.ForEach(source.GetFiles(searchPattern, SearchOption.AllDirectories), file =>
+                                                                                          {
+                                                                                            var targetFile = target.CombineTo<FileInfo>(file.Name);
+                                                                                            file.CopyTo(targetFile.FullName, true);
+                                                                                          });
+    }
+  }
 }
--- Trunk/SitecoreInstaller.Framework/IO/Robocopy.cs
+++ Trunk/SitecoreInstaller.Framework/IO/Robocopy.cs
@@ -5,43 +5,45 @@ using System.Text;
 
 namespace SitecoreInstaller.Framework.IO
 {
-    using SitecoreInstaller.Framework.System;
+  using SitecoreInstaller.Framework.System;
 
-    using global::System.Diagnostics.Contracts;
-    using global::System.IO;
+  using global::System.Diagnostics.Contracts;
+  using global::System.IO;
 
-    public class Robocopy : CommandPrompt
+  public class Robocopy : CommandPrompt
+  {
+    private const string _FileName = @"Robocopy";
+    private const string _SourceDestinationFormat = _FileName + @" ""{0}"" ""{1}"" /NP ";
+    private const string _IncludeSubfoldersSwitch = " /e ";
+    private const string _MoveSwitch = " /move ";
+
+    private void Run(DirectoryInfo source, DirectoryInfo target, string switches)
     {
-        private const string _FileName = @"Robocopy";
-        private const string _SourceDestinationFormat = _FileName + @" ""{0}"" ""{1}"" /NP ";
-        private const string _IncludeSubfoldersSwitch = " /e ";
-        private const string _MoveSwitch = " /move ";
+      Contract.Requires<ArgumentNullException>(target != null);
+      Contract.Requires<ArgumentNullException>(source != null);
 
-        private void Run(DirectoryInfo source, DirectoryInfo target, string switches)
-        {
-            Contract.Requires<ArgumentNullException>(target != null);
-            Contract.Requires<ArgumentNullException>(source != null);
-
-            var command = string.Format(_SourceDestinationFormat, source.FullName, target.FullName);
-            command += switches;
-            Run(command);
-        }
-
-        public void Copy(DirectoryInfo source, DirectoryInfo target, DirCopyOptions dirCopyOptions)
-        {
-            var switches = string.Empty;
-            if (dirCopyOptions == DirCopyOptions.IncludeSubDirectories)
-                switches = _IncludeSubfoldersSwitch;
-            Run(source, target, switches);
-        }
-        public void Move(DirectoryInfo source, DirectoryInfo target)
-        {
-            Run(source, target, _IncludeSubfoldersSwitch + _MoveSwitch);
-            source.Delete(true);
-        }
-        public void Move(FileInfo file, DirectoryInfo target)
-        {
-            Run(file.Directory, target, file.Name + _MoveSwitch);
-        }
+      var command = string.Format(_SourceDestinationFormat, source.FullName, target.FullName);
+      command += switches;
+      Run(command);
     }
+
+    public void Copy(DirectoryInfo source, DirectoryInfo target, DirCopyOptions dirCopyOptions)
+    {
+      var switches = string.Empty;
+      if (dirCopyOptions == DirCopyOptions.IncludeSubDirectories)
+        switches = _IncludeSubfoldersSwitch;
+      Run(source, target, switches);
+    }
+    public void Move(DirectoryInfo source, DirectoryInfo target)
+    {
+      Run(source, target, _IncludeSubfoldersSwitch + _MoveSwitch);
+      source.Refresh();
+      if (source.Exists)
+        source.Delete(true);
+    }
+    public void Move(FileInfo file, DirectoryInfo target)
+    {
+      Run(file.Directory, target, file.Name + _MoveSwitch);
+    }
+  }
 }
