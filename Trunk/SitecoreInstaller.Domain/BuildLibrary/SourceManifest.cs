f370b1ab2cfd4379385352881974f4cf8c6da55a
rzmoz <rzmoz@users.noreply.github.com>
2013-05-31 10:48:46 +0200

    Changed source manifest to use attributes in config file

--- Trunk/SitecoreInstaller.Domain/BuildLibrary/SourceManifest.cs
+++ Trunk/SitecoreInstaller.Domain/BuildLibrary/SourceManifest.cs
@@ -1,21 +1,27 @@
 ﻿namespace SitecoreInstaller.Domain.BuildLibrary
 {
+  using System.Xml.Serialization;
 
-    public class SourceManifest
+  public class SourceManifest
+  {
+    public SourceManifest()
     {
-        public SourceManifest()
-        {
-        }
-
-        public SourceManifest(string name, string type, string parameters)
-        {
-            Name = name ?? string.Empty;
-            Type = type ?? string.Empty;
-            Parameters = parameters ?? string.Empty;
-        }
-
-        public string Name { get; set; }
-        public string Type { get; set; }
-        public string Parameters { get; set; }
     }
+
+    public SourceManifest(string name, string type, string parameters)
+    {
+      Name = name ?? string.Empty;
+      Type = type ?? string.Empty;
+      Parameters = parameters ?? string.Empty;
+    }
+
+    [XmlAttribute]
+    public string Name { get; set; }
+    
+    [XmlAttribute]
+    public string Type { get; set; }
+    
+    [XmlAttribute]
+    public string Parameters { get; set; }
+  }
 }
