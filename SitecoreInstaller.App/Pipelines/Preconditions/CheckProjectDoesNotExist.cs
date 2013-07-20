4316b89d1ec79c20340970301b5c9e9d22069ebf
rzmoz <rzmoz@users.noreply.github.com>
2013-07-20 13:04:12 +0200

    cc

--- SitecoreInstaller.App/Pipelines/Preconditions/CheckProjectDoesNotExist.cs
+++ SitecoreInstaller.App/Pipelines/Preconditions/CheckProjectDoesNotExist.cs
@@ -2,9 +2,9 @@
 {
   public class CheckProjectDoesNotExist : CheckProjectExists
   {
-    public override bool InnerEvaluate(object sender, StepEventArgs args)
+    public override bool InnerEvaluate(object sender, PipelineEventArgs args)
     {
-      if (base.Evaluate(sender, args) == false)
+      if (base.InnerEvaluate(sender, args) == false)
         return true;
 
       ErrorMessage = string.Format("Project '{0}' already exists.\r\nPlease delete first or choose another project name for this installation.\r\n\r\nLocation: {1}", args.ProjectSettings.ProjectName, args.ProjectSettings.ProjectFolder);
