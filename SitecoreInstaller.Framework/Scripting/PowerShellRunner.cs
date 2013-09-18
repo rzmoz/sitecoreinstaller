f9f77f1a11d0990554e81376600e0063b7598127
rzmoz <rzmoz@users.noreply.github.com>
2013-09-18 15:08:50 +0200

    Fixed so you don't have to change global execution policy for powershell to run scripts. Scripts now run in execution policy bypass for the session

--- SitecoreInstaller.Framework/Scripting/PowerShellRunner.cs
+++ SitecoreInstaller.Framework/Scripting/PowerShellRunner.cs
@@ -1,42 +1,49 @@
 ﻿namespace SitecoreInstaller.Framework.Scripting
 {
-  using System;
-  using System.Collections.Generic;
-  using System.IO;
-  using System.Management.Automation;
-  using System.Text;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Management.Automation;
+    using System.Text;
 
-  public class PowerShellRunner
-  {
-    public string RunPowerShellFunction(string method, KeyValuePair<string, object> arg, FileInfo script)
+    public class PowerShellRunner
     {
-      if (method == null) { throw new ArgumentNullException("method"); }
-      if (script == null) { throw new ArgumentNullException("scriptPath"); }
-
-
-      if (File.Exists(script.FullName) == false)
-        throw new ArgumentException("Script not found at:" + script.FullName);
-
-      using (var ps = PowerShell.Create())
-      {
-        ps.AddScript(". '" + script.FullName + "'", false);
-        ps.Invoke();
-
-        ps.Commands.Clear();
-
-        ps.AddCommand(method).AddParameter(arg.Key, arg.Value);
-
-        var results = ps.Invoke();
-
-        var resultString = new StringBuilder();
-
-        foreach (var psObject in results)
+        public string RunPowerShellFunction(string method, KeyValuePair<string, object> arg, FileInfo script)
         {
-          resultString.Append(psObject + Environment.NewLine);
+            if (method == null) { throw new ArgumentNullException("method"); }
+            if (script == null) { throw new ArgumentNullException("scriptPath"); }
+
+
+            if (File.Exists(script.FullName) == false)
+                throw new ArgumentException("Script not found at:" + script.FullName);
+
+            using (var ps = PowerShell.Create())
+            {
+                AddExecutionPolicy(ps);
+                ps.AddScript(". '" + script.FullName + "'");
+                ps.Invoke();
+
+                ps.Commands.Clear();
+
+                AddExecutionPolicy(ps);
+                ps.AddCommand(method).AddParameter(arg.Key, arg.Value);
+
+                var results = ps.Invoke();
+
+                var resultString = new StringBuilder();
+
+                foreach (var psObject in results)
+                {
+                    resultString.Append(psObject + Environment.NewLine);
+                }
+
+                return resultString.ToString().TrimEnd(Environment.NewLine.ToCharArray());
+            }
         }
 
-        return resultString.ToString().TrimEnd(Environment.NewLine.ToCharArray());
-      }
+        private void AddExecutionPolicy(PowerShell ps)
+        {
+            ps.AddScript("Set-ExecutionPolicy Bypass -Scope Process");
+        }
     }
-  }
 }
