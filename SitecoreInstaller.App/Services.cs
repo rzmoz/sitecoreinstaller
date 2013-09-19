066d8f64dc04091130f789b9a3098064a146140c
rzmoz <rzmoz@users.noreply.github.com>
2013-09-19 22:27:38 +0200

    Added prompt on start for simple or advanced view

--- SitecoreInstaller.App/Services.cs
+++ SitecoreInstaller.App/Services.cs
@@ -11,103 +11,103 @@ using SitecoreInstaller.Framework.Sys;
 
 namespace SitecoreInstaller.App
 {
-  using System;
-  using System.Threading.Tasks;
-  using SitecoreInstaller.App.Pipelines;
-  using SitecoreInstaller.Domain.Pipelines;
+    using System;
+    using System.Threading.Tasks;
+    using SitecoreInstaller.App.Pipelines;
+    using SitecoreInstaller.Domain.Pipelines;
 
-  public static class Services
-  {
-    static Services()
+    public static class Services
     {
-      Pipelines = new PipelineService();
-      PowerShellScripts = new PowerShellScriptService();
-      Website = new WebsiteService();
-      IisManagement = new IisManagementService();
-      PipelineWorker = new PipelineWorker();
-      SourceManifests = new SourceManifestRepository(new FileInfo(AppConstants.SourcesConfigFileName));
-      Sql = new SqlService();
-      Mongo = new MongoService();
+        static Services()
+        {
+            Pipelines = new PipelineService();
+            PowerShellScripts = new PowerShellScriptService();
+            Website = new WebsiteService();
+            IisManagement = new IisManagementService();
+            PipelineWorker = new PipelineWorker();
+            SourceManifests = new SourceManifestRepository(new FileInfo(AppConstants.SourcesConfigFileName));
+            Sql = new SqlService();
+            Mongo = new MongoService();
+        }
+
+        public static async Task LoadUserPreferencesAsync()
+        {
+            await Task.Factory.StartNew(LoadUserPreferences);
+        }
+        public static async Task InitAsync()
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                //init before initializing build library
+                SourceManifests.Init();
+
+                InitBuildLibrary();
+
+                InitProjects();
+            });
+        }
+
+        private static void InitBuildLibrary()
+        {
+            var localBuildLibrary = new WindowsFileSystemSource(string.Empty)
+            {
+                Parameters = UserPreferences.Properties.LocalBuildLibrary
+            };
+            if (BuildLibrary == null)
+            {
+                BuildLibrary = new LocalSourceRepository(localBuildLibrary, SourceManifests.Enabled.Select(Create));
+            }
+            else
+            {
+                ((LocalSourceRepository)BuildLibrary).Init(localBuildLibrary, SourceManifests.Enabled.Select(Create));
+            }
+
+            BuildLibrary.Update();
+        }
+
+        private static void LoadUserPreferences()
+        {
+            UserPreferences = new ConfigFile<UserPreferencesConfig>(new FileInfo(AppConstants.UserPreferencesFileName));
+            UserPreferences.Updated += UserPreferences_Updated;
+
+            if (File.Exists(AppConstants.UserPreferencesFileName) == false)
+                return;
+
+            new BuildLibraryFolders(UserPreferences.Properties.LocalBuildLibrary).Create();
+            new DirectoryInfo(UserPreferences.Properties.ProjectsFolder).Create();
+        }
+
+        static void UserPreferences_Updated(object sender, GenericEventArgs<UserPreferencesConfig> e)
+        {
+            InitProjects();
+            InitBuildLibrary();
+        }
+
+        private static void InitProjects()
+        {
+            Projects = new ProjectsService(UserPreferences.Properties.ProjectsFolder);
+        }
+
+        private static ISource Create(SourceManifest sourceManifest)
+        {
+            var sourceFactory = new SourceFactory();
+            var sourceInstance = sourceFactory.Create<ISource>(sourceManifest.Type, sourceManifest.Name);
+            sourceInstance.Parameters = sourceManifest.Parameters;
+            sourceInstance.Enabled = sourceManifest.Enabled;
+            return sourceInstance;
+        }
+
+        private static SourceManifestRepository SourceManifests { get; set; }
+        public static ConfigFile<UserPreferencesConfig> UserPreferences { get; private set; }
+
+        public static PipelineService Pipelines { get; private set; }
+        public static PowerShellScriptService PowerShellScripts { get; private set; }
+        public static ISourceRepository BuildLibrary { get; private set; }
+        public static ProjectsService Projects { get; private set; }
+        public static IWebsiteService Website { get; private set; }
+        public static IIisManagementService IisManagement { get; private set; }
+        public static SqlService Sql { get; private set; }
+        public static MongoService Mongo { get; private set; }
+        public static PipelineWorker PipelineWorker { get; private set; }
     }
-
-    public static async Task InitAsync()
-    {
-      await Task.Factory.StartNew(() =>
-      {
-        LoadUserPreferences();
-
-        //init before initializing build library
-        SourceManifests.Init();
-
-        InitBuildLibrary();
-
-        InitProjects();
-
-
-      });
-    }
-
-    private static void InitBuildLibrary()
-    {
-      var localBuildLibrary = new WindowsFileSystemSource(string.Empty)
-      {
-        Parameters = UserPreferences.Properties.LocalBuildLibrary
-      };
-      if (BuildLibrary == null)
-      {
-        BuildLibrary = new LocalSourceRepository(localBuildLibrary, SourceManifests.Enabled.Select(Create));
-      }
-      else
-      {
-        ((LocalSourceRepository)BuildLibrary).Init(localBuildLibrary, SourceManifests.Enabled.Select(Create));
-      }
-
-      BuildLibrary.Update();
-    }
-
-    private static void LoadUserPreferences()
-    {
-      UserPreferences = new ConfigFile<UserPreferencesConfig>(new FileInfo(AppConstants.UserPreferencesFileName));
-      UserPreferences.Updated += UserPreferences_Updated;
-
-      if (File.Exists(AppConstants.UserPreferencesFileName) == false)
-        return;
-
-      new BuildLibraryFolders(UserPreferences.Properties.LocalBuildLibrary).Create();
-      new DirectoryInfo(UserPreferences.Properties.ProjectsFolder).Create();
-    }
-
-    static void UserPreferences_Updated(object sender, GenericEventArgs<UserPreferencesConfig> e)
-    {
-      InitProjects();
-      InitBuildLibrary();
-    }
-
-    private static void InitProjects()
-    {
-      Projects = new ProjectsService(UserPreferences.Properties.ProjectsFolder);
-    }
-
-    private static ISource Create(SourceManifest sourceManifest)
-    {
-      var sourceFactory = new SourceFactory();
-      var sourceInstance = sourceFactory.Create<ISource>(sourceManifest.Type, sourceManifest.Name);
-      sourceInstance.Parameters = sourceManifest.Parameters;
-      sourceInstance.Enabled = sourceManifest.Enabled;
-      return sourceInstance;
-    }
-
-    private static SourceManifestRepository SourceManifests { get; set; }
-    public static ConfigFile<UserPreferencesConfig> UserPreferences { get; private set; }
-
-    public static PipelineService Pipelines { get; private set; }
-    public static PowerShellScriptService PowerShellScripts { get; private set; }
-    public static ISourceRepository BuildLibrary { get; private set; }
-    public static ProjectsService Projects { get; private set; }
-    public static IWebsiteService Website { get; private set; }
-    public static IIisManagementService IisManagement { get; private set; }
-    public static SqlService Sql { get; private set; }
-    public static MongoService Mongo { get; private set; }
-    public static PipelineWorker PipelineWorker { get; private set; }
-  }
 }
--- SitecoreInstaller/frmSplashScreen.cs
+++ SitecoreInstaller/frmSplashScreen.cs
@@ -7,47 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SitecoreInstaller.App;
+using SitecoreInstaller.UI;
 
 namespace SitecoreInstaller
 {
-  using SitecoreInstaller.App;
-  using SitecoreInstaller.UI;
-  using SitecoreInstaller.UI.Viewport;
-
-  public partial class FrmSplashScreen : Form
-  {
-    public FrmSplashScreen()
+    public partial class FrmSplashScreen : Form
     {
-      InitializeComponent();
+        public FrmSplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            this.BackColor = Styles.Controls.BackColor;
+            this.CenterToScreen();
+            timer1.Start();//start timer as one of the first tasks to start animation!
+            await Services.LoadUserPreferencesAsync();
+
+            Task userPreferences = null;
+            if (Services.UserPreferences.Properties.PromptForUserSettings)
+            {
+                userPreferences = Task.Factory.StartNew(() => Services.UserPreferences.Properties.AdvancedView = !UiServices.Dialogs.UserAccept("Do you want to use simple view? (no means advanced view with a lot of buttons"));
+            }
+            await Services.InitAsync();
+            
+            Services.UserPreferences.Properties.PromptForUserSettings = false;
+            Services.UserPreferences.Save();
+            
+            if (userPreferences != null)
+                userPreferences.Wait();
+            var frmMain = new FrmMain();
+            frmMain.Closed += (sender, args) => this.Close();
+            frmMain.Init();
+            this.Hide();
+            frmMain.Show();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //if we reached right side, then we go back
+            var rightProximity = this.Width - picLogo.Right;
+            var leftProximity = picLogo.Left;
+
+            if (rightProximity < 50 || leftProximity < 50)
+                this.movePicOffSet = movePicOffSet * -1;
+
+            picLogo.Left += movePicOffSet;
+        }
+
+        private int movePicOffSet = 3;
     }
-
-    protected async override void OnLoad(EventArgs e)
-    {
-      base.OnLoad(e);
-      this.BackColor = Styles.Controls.BackColor;
-      this.CenterToScreen();
-      timer1.Start();//start timer as one of the first tasks to start animation!
-      
-      await Services.InitAsync();
-      var frmMain = new FrmMain();
-      frmMain.Closed += (sender, args) => this.Close();
-      frmMain.Init();
-      this.Hide();
-      frmMain.Show();
-    }
-
-    private void timer1_Tick(object sender, EventArgs e)
-    {
-      //if we reached right side, then we go back
-      var rightProximity = this.Width - picLogo.Right;
-      var leftProximity = picLogo.Left;
-
-      if (rightProximity < 50 || leftProximity < 50)
-        this.movePicOffSet = movePicOffSet * -1;
-      
-      picLogo.Left += movePicOffSet;
-    }
-
-    private int movePicOffSet = 3;
-  }
 }
