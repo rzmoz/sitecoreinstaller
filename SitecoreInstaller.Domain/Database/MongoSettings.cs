3411375f24baa9e601adc2ba947069f611c46651
rzmoz <rzmoz@users.noreply.github.com>
2013-10-14 12:01:00 +0200

    fixed so mongo db settings verifies that a collection can be created and dropped

--- SitecoreInstaller.Domain/Database/MongoSettings.cs
+++ SitecoreInstaller.Domain/Database/MongoSettings.cs
@@ -9,37 +9,44 @@ using SitecoreInstaller.Framework.Diagnostics;
 
 namespace SitecoreInstaller.Domain.Database
 {
-    public class MongoSettings
+  public class MongoSettings
+  {
+    public MongoSettings()
     {
-        public MongoSettings()
-        {
-            Username = string.Empty;
-            Password = string.Empty;
-            Endpoint = "localhost";
-            Port = 27017;
-        }
-
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Endpoint { get; set; }
-        public int Port { get; set; }
-
-        public bool TestConnection()
-        {
-          try
-          {
-            var conStr = new MongoConnectionString(this).Value;
-            var client = new MongoClient(conStr);
-            var server = client.GetServer();
-            server.Ping();
-            return true;
-          }
-          catch (Exception e)
-          {
-            Log.This.Debug(e.ToString());
-            Log.This.Error(e.Message);
-            return false;
-          }
-        }
+      Username = string.Empty;
+      Password = string.Empty;
+      Endpoint = "localhost";
+      Port = 27017;
     }
+
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Endpoint { get; set; }
+    public int Port { get; set; }
+
+    public bool TestConnection()
+    {
+      try
+      {
+        var conStr = new MongoConnectionString(this).Value;
+        var client = new MongoClient(conStr);
+        var server = client.GetServer();
+        server.Ping();
+        var dbSettings = new MongoDatabaseSettings();
+        var db = new MongoDatabase(server, "SitecoreInstallerVerifyCredentialsDatabase", dbSettings);
+        const string testCollectionName = "test";
+        if (db.CollectionExists(testCollectionName))
+          db.DropCollection(testCollectionName);
+        db.CreateCollection(testCollectionName);
+        db.Drop();
+        return true;
+      }
+      catch (Exception e)
+      {
+        Log.This.Debug(e.ToString());
+        Log.This.Error(e.Message);
+        return false;
+      }
+    }
+  }
 }
