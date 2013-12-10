5288ef15763ac62055ed5c1161a442ab87c1ac02
rzmoz <rzmoz@users.noreply.github.com>
2013-12-10 12:04:07 +0100

    revmoed check for sql settings on install when doing a client install

--- SitecoreInstaller.App/Pipelines/Preconditions/CheckSqlConnection.cs
+++ SitecoreInstaller.App/Pipelines/Preconditions/CheckSqlConnection.cs
@@ -1,13 +1,19 @@
-﻿namespace SitecoreInstaller.App.Pipelines.Preconditions
+﻿using SitecoreInstaller.Domain;
+
+namespace SitecoreInstaller.App.Pipelines.Preconditions
 {
-  public class CheckSqlConnection : Precondition<PipelineApplicationEventArgs>
-  {
-    public override bool InnerEvaluate(object sender, PipelineApplicationEventArgs args)
+    public class CheckSqlConnection : Precondition<PipelineApplicationEventArgs>
     {
-      if (args.ProjectSettings.Sql.TestConnection())
-        return true;
-      ErrorMessage = "Sql settings are not properly set. Check under preferences";
-      return false;
+        public override bool InnerEvaluate(object sender, PipelineApplicationEventArgs args)
+        {
+            //we don't verify sql settings on client install
+            if (args.ProjectSettings.InstallType == InstallType.Client)
+                return true;
+
+            if (args.ProjectSettings.Sql.TestConnection())
+                return true;
+            ErrorMessage = "Sql settings are not properly set. Check under preferences";
+            return false;
+        }
     }
-  }
 }
