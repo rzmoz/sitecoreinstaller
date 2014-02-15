ac88e5d2a23781d261c3d8b799647d0a19c62037
rzmoz <rzmoz@users.noreply.github.com>
2014-02-15 14:12:59 +0100

    added ndepend project

--- SitecoreInstaller/BootWizardBooter.cs
+++ SitecoreInstaller/BootWizardBooter.cs
@@ -13,9 +13,7 @@ namespace SitecoreInstaller
 
         public override Task<bool> InitAsync()
         {
-            var wizardcontrol = Control as BootWizardControl;
-            if (wizardcontrol == null)
-                throw new ApplicationException("not the right type. This shouldn't be possible");
+            var wizardcontrol = (BootWizardControl)Control;
             return wizardcontrol.InitAsync();
         }
     }
