18412a94057da5467c5abe0bdf05baacb7c68537
rzmoz <rzmoz@users.noreply.github.com>
2014-03-05 13:38:41 +0100

    Added inital setup for createing bacpacs and a sitecore cspkg

---
+++ SitecoreInstaller.App/Pipelines/Steps/Azure/PrepDatabasesForBacpacFormat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SitecoreInstaller.App.Pipelines.Steps.Azure
+{
+    public class PrepDatabasesForBacpacFormat : Step<PipelineApplicationEventArgs>
+    {
+        protected override void InnerInvoke(object sender, PipelineApplicationEventArgs args)
+        {
+            var connectionStrings = args.ProjectSettings.ProjectFolder.Website.AppConfig.ConnectionStringsConfigFile;
+                
+        }
+    }
+}
---
+++ SitecoreInstaller.Azure/Packaging/CsPkg/ScaaSEntryPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using CSharp.Basics.Sys.Tasks;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace SitecoreInstaller.Azure.Packaging.CsPkg
+{
+    public class ScaaSEntryPoint : RoleEntryPoint
+    {
+        //http://blogs.msdn.com/b/windowsazure/archive/2013/01/14/the-right-way-to-handle-azure-onstop-events.aspx
+        public override void OnStop()
+        {
+            Trace.TraceInformation("OnStop called webrole");
+            var prc = new PerformanceCounter("ASP.NET", "Requests Current", "");
+
+            while (true)
+            {
+                var rc = prc.NextValue();
+                Trace.TraceInformation("ASP.NET Requests Current: " + rc.ToString());
+                if (rc <= 0)
+                    break;
+                Wait.For("1s");
+            }
+        }
+    }
+}
