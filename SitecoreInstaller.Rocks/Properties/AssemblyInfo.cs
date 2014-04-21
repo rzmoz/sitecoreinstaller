21155867cc1d4c279559e60c6e7c78d39b9a1734
rzmoz <rzmoz@users.noreply.github.com>
2014-04-21 21:12:13 +0200

    aupdated nuspec for Rocks

--- SitecoreInstaller.Rocks/Properties/AssemblyInfo.cs
+++ SitecoreInstaller.Rocks/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sitecore.Rocks.Installer")]
+[assembly: AssemblyTitle("Sitecore.Installer.Rocks")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sitecore.Rocks.Installer")]
+[assembly: AssemblyProduct("SitecoreInstaller.Rocks")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
