13ca682e470ece7bf1698caac8f131d7cc73b35f
rzmoz <rzmoz@users.noreply.github.com>
2014-05-02 14:06:51 +0200

    removed azure code completely - not going to support that

--- SitecoreInstaller.Azure/Packaging/Databases/AddClusteredIndexesSqlCoreOnly.sql
+++
@@ -1,5 +0,0 @@
---Core Only
-IF NOT EXISTS (SELECT * FROM sys.indexes WHERE name='ClusteredIndexForAzure' AND object_id = OBJECT_ID('RolesInRoles'))
-BEGIN
-    CREATE CLUSTERED INDEX ClusteredIndexForAzure ON RolesInRoles(Id)
-END
