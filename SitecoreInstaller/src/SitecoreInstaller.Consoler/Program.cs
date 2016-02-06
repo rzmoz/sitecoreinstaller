fcb05966ae2849ad0b5c5dd1e6bad6166ab3a374
rzmoz <rzmoz@users.noreply.github.com>
2016-02-06 16:26:12 +0800

    added console app for testing

---
+++ SitecoreInstaller/src/SitecoreInstaller.Consoler/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SitecoreInstaller.Consoler
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
