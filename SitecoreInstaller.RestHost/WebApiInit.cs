4aeee64bf9cab984f31a9fd0cf699ecad1acb348
rzmoz <rzmoz@users.noreply.github.com>
2016-10-19 16:57:31 +0800

    added json serialization settings to web api init

--- SitecoreInstaller.RestHost/WebApiInit.cs
+++ SitecoreInstaller.RestHost/WebApiInit.cs
@@ -1,6 +1,10 @@
-﻿using System.Web.Http;
+﻿using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Http;
 using System.Web.Http.ExceptionHandling;
 using Autofac.Integration.WebApi;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using NLog;
 using Owin;
 using SitecoreInstaller.Runtime;
@@ -24,6 +28,23 @@ namespace SitecoreInstaller.RestHost
 
             logger.Debug($"{nameof(config.IncludeErrorDetailPolicy)}: {config.IncludeErrorDetailPolicy }");
             logger.Debug($"{nameof(config.DependencyResolver)}: {config.DependencyResolver.GetType().FullName}");
+            
+            // Json settings
+            config.Formatters.JsonFormatter.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Formatting = Formatting.None,
+                NullValueHandling = NullValueHandling.Include,
+                TypeNameHandling = TypeNameHandling.None
+            };
+
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/plain"));
 
             config.EnsureInitialized();
 
