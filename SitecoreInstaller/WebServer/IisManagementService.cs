7ff7d5c5ac25ceb73d6f05da3822bf1d2cb68757
rzmoz <rzmoz@users.noreply.github.com>
2016-10-19 14:02:41 +0800

    cc

--- SitecoreInstaller/WebServer/IisManagementService.cs
+++ SitecoreInstaller/WebServer/IisManagementService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using DotNet.Basics.Collections;
 using DotNet.Basics.IO;
 using Microsoft.Web.Administration;
 using NLog;
@@ -17,61 +19,68 @@ namespace SitecoreInstaller.WebServer
 
         public void CreateApplication(IisApplicationSettings settings)
         {
-            using (var iisManager = new ServerManager())
+            _logger.Trace($"Creating Iis Application: {settings.Name}...");
+            using (var iisManager = new IisManager(settings))
             {
-                iisManager.CreateAppPool(settings.AppPoolSettings);
-                iisManager.CreateSite(settings.SiteSettings, settings.AppPoolSettings);
-                iisManager.CommitChanges();
+                CreateAppPool(iisManager.ServerManager, settings.AppPoolSettings);
+                CreateSite(iisManager.ServerManager, settings.SiteSettings, settings.AppPoolSettings);
             }
             settings.SiteSettings.SiteRoot.CreateIfNotExists();
+            _logger.Debug($"{nameof(settings.SiteSettings.SiteRoot)} created: {settings.SiteSettings.SiteRoot.FullName}");
             settings.SiteSettings.IisLogFilesDir.CreateIfNotExists();
+            _logger.Trace($"Iis Application created: {settings.Name}");
         }
 
         public void DeleteApplication(IisApplicationSettings settings)
         {
-            using (var iisManager = new ServerManager())
+            _logger.Trace($"Deleting Iis Application: {settings.Name}...");
+            using (var iisManager = new IisManager(settings))
             {
-                iisManager.DeleteSite(settings.SiteSettings);
-                iisManager.DeleteAppPool(settings.AppPoolSettings);
-                iisManager.CommitChanges();
+                iisManager.Site(site => site?.Delete());
+                _logger.Debug($"Website deleted: {settings.SiteSettings.Name}");
+                iisManager.AppPool(appPool =>
+                {
+                    appPool?.WorkerProcesses.ParallelForEach(wp => Process.GetProcessById(wp.ProcessId).Kill());
+                    appPool?.Delete();
+                    _logger.Debug($"App pool deleted: {settings.AppPoolSettings.Name}");
+                });
             }
             settings.SiteSettings.SiteRoot.DeleteIfExists();
+            _logger.Debug($"{nameof(settings.SiteSettings.SiteRoot)} deleted: {settings.SiteSettings.SiteRoot.FullName}");
             settings.SiteSettings.IisLogFilesDir.DeleteIfExists();
+            _logger.Trace($"Iis Application deleted: {settings.Name}");
         }
 
-        public void RecycleApplication(string applicationName)
+        public void RecycleApplication(IisApplicationSettings settings)
         {
-            using (var iisManager = new ServerManager())
+            using (var iisManager = new IisManager(settings))
             {
-                iisManager.ApplicationPools[applicationName]?.Recycle();
-                iisManager.CommitChanges();
+                iisManager.AppPool(appPool => appPool?.Recycle());
             }
         }
 
         public void StartApplication(IisApplicationSettings settings)
         {
-            using (var iisManager = new ServerManager())
+            using (var iisManager = new IisManager(settings))
             {
-                var site = iisManager.GetSite(settings.SiteSettings);
-                site?.Start();
-                _logger.Trace($"Iis Site started: {settings.SiteSettings.Name}");
-                var appPool = iisManager.GetAppPool(settings.AppPoolSettings);
-                appPool?.Start();
-                _logger.Trace($"Iis App Pool started: {settings.AppPoolSettings.Name}");
+                iisManager.AppPool(appPool => appPool?.Start());
+                _logger.Debug($"Iis App Pool started: {settings.AppPoolSettings.Name}");
+                iisManager.Site(site => site?.Start());
+                _logger.Debug($"Iis Site started: {settings.SiteSettings.Name}");
             }
+            _logger.Trace($"Iis Application started: {settings.Name}");
         }
 
         public void StopApplication(IisApplicationSettings settings)
         {
-            using (var iisManager = new ServerManager())
+            using (var iisManager = new IisManager(settings))
             {
-                var site = iisManager.GetSite(settings.SiteSettings);
-                site?.Stop();
-                _logger.Trace($"Iis Site stopped: {settings.SiteSettings.Name}");
-                var appPool = iisManager.GetAppPool(settings.AppPoolSettings);
-                appPool?.Stop();
-                _logger.Trace($"Iis App Pool stopped: {settings.AppPoolSettings.Name}");
+                iisManager.Site(site => site?.Stop());
+                _logger.Debug($"Iis Site stopped: {settings.SiteSettings.Name}");
+                iisManager.AppPool(appPool => appPool?.Stop());
+                _logger.Debug($"Iis App Pool stopped: {settings.AppPoolSettings.Name}");
             }
+            _logger.Debug($"Iis Application stopped: {settings.Name}");
         }
 
         public bool BindingExists(string bindingCandidate)
@@ -98,6 +107,35 @@ namespace SitecoreInstaller.WebServer
         public PreflightCheckResult Assert()
         {
             return new PreflightCheckResult();
+        }
+
+
+        public void CreateSite(ServerManager iisManager, SiteSettings sitesettings, AppPoolSettings appPoolSettings)
+        {
+            var site = iisManager.Sites.Add(sitesettings.Name, "http", sitesettings.BindingInformation, sitesettings.SiteRoot.FullName);
+            site.ApplicationDefaults.ApplicationPoolName = appPoolSettings.Name;
+            site.LogFile.Directory = sitesettings.IisLogFilesDir.FullName;
+
+            _logger.Debug($"Site home directory set to '{sitesettings.SiteRoot.FullName}'");
+            _logger.Debug($"Site log dir set to '{sitesettings.IisLogFilesDir.FullName}'");
+            _logger.Debug($"Site App Pool set to '{appPoolSettings.Name}'");
+            _logger.Debug($"Iis Site created: {sitesettings.Name}");
+        }
+
+        public void CreateAppPool(ServerManager iisManager, AppPoolSettings settings)
+        {
+            var appPool = iisManager.ApplicationPools.Add(settings.Name);
+            appPool.ManagedRuntimeVersion = settings.ManagedRuntimeVersion;
+            appPool.ManagedPipelineMode = settings.ManagedPipelineMode;
+            appPool.Enable32BitAppOnWin64 = settings.Enable32BitAppOnWin64;
+            appPool.ProcessModel.PingingEnabled = settings.PingEnabled;
+            appPool.ProcessModel.IdentityType = settings.ProcessModelIdentityType;
+
+
+            _logger.Debug("App pool runtime version set to {0}", appPool.ManagedRuntimeVersion);
+            _logger.Debug("App pool pipeline mode set to {0}", appPool.ManagedPipelineMode);
+            _logger.Debug("App pool identity set to {0}", appPool.ProcessModel.IdentityType);
+            _logger.Debug($"App pool created: {settings.Name}");
         }
     }
 }
---
+++ SitecoreInstaller/WebServer/IisManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Web.Administration;
+
+namespace SitecoreInstaller.WebServer
+{
+    public class IisManager : IDisposable
+    {
+        private readonly IisApplicationSettings _settings;
+        
+
+        public IisManager(IisApplicationSettings settings)
+        {
+            _settings = settings;
+            ServerManager = new ServerManager();
+        }
+
+        public ServerManager ServerManager { get; }
+
+        public void AppPool(Action<ApplicationPool> appPoolAction)
+        {
+            appPoolAction(ServerManager.ApplicationPools[_settings.AppPoolSettings.Name]);
+        }
+        public void Site(Action<Site> siteAction)
+        {
+            siteAction(ServerManager.Sites[_settings.SiteSettings.Name]);
+        }
+
+        public void Dispose()
+        {
+            ServerManager?.CommitChanges();
+            ServerManager?.Dispose();
+        }
+    }
+}
