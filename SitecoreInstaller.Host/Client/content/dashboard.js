e669dc6183941d1182f58cf6a5b00af4824ef182
rzmoz <rzmoz@users.noreply.github.com>
2016-11-04 17:17:06 +0800

    added dashboard page

---
+++ SitecoreInstaller.Host/Client/content/dashboard.js
@@ -0,0 +1,11 @@
+﻿var dashboard = {
+    iGetTitle: function () {
+        return "Local IIS Deployments";
+    },
+    iLoad: function (doneCallback) {
+        var deploymentsTable = $('#table-local-deployments');
+        localDeployments.loadAllInfos(deploymentsTable);
+
+        doneCallback();
+    }
+}
--- SitecoreInstaller.Host/Client/js/host.js
+++ SitecoreInstaller.Host/Client/js/host.js
@@ -8,6 +8,55 @@
     }
 }
 
+var localDeployments = {
+    loadAllInfos: function (dataTableElement) {
+        dataTableElement.on("click", "button.del-deployment", function (e) {
+
+            var depName = this.name;
+
+            $.confirm({
+                title: "Delete " + depName,
+                content: 'Are you sure you want to delete ' + depName + '?',
+                confirm: function () {
+                    alert('deleting:' + depName);
+                    localDeployments.delete(depName);
+                }
+            });
+        });
+
+        localDeployments.getAll(function (json) {
+            $.each(json,
+                function (index, info) {
+                    dataTableElement.children('tbody').append('<tr><td>' + info.name + '</td><td>' + info.sitecore.replace('Sitecore ', '') + '</td><td><button type="button" class="btn btn-danger del-deployment" name="' + info.name + '">Delete</button></td></tr>');
+                });
+
+
+
+            console.log('Deployments loaded:' + JSON.stringify(json));
+        });
+    },
+
+
+    getAll: function (callback) {
+        var uri = "/api/local/deployments";
+        $.getJSON(uri, callback);
+    },
+    delete: function (name, responseCallback) {
+
+        $.delete('/api/local/deployments/' + name, '', responseCallback);
+    },
+    put: function (name, sitecore, license, modules, responseCallback) {
+        var body = {
+            name: name,
+            sitecore: sitecore,
+            license: license,
+            modules: []
+        };
+        $.put('/api/local/deployments', body, responseCallback);
+    }
+}
+
+
 jQuery.each(["put", "delete"], function (i, method) {
     jQuery[method] = function (url, data, callback) {
 
